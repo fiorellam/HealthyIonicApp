f36e74c609ee3615c6742a95fd3eabc7699e035c
unknown <fiorellaroma@hotmail.com>
2020-06-05 09:41:40 -0700

    Arreglo de bmi

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RecipesService } from 'src/app/services/recipes.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private recipesService: RecipesService) {
+    this.recipesService.getRecipesFoodNutrition();
+  }
 
 }
--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { IResponseRecipes } from '../interfaces/IResponseRecipes';
+import { map } from 'rxjs/operators';
 
 const URL = environment.url;
 @Injectable({
@@ -17,5 +18,25 @@ export class RecipesService {
   getRecipesServer(){
     this.paginaRecipes ++;
     return this.http.get<IResponseRecipes>(`${URL}/recipes/?pagina=${this.paginaRecipes}`)
+  }
+
+  getQuery(query:string){
+    const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${query}`
+    const headers = new HttpHeaders({
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+	    "x-rapidapi-key": "87cca58015mshe835e7264fd3118p1b712cjsn7c517543a14f",
+    });
+    return this.http.get(url, {headers});
+  }
+
+  getRecipesFoodNutrition(){
+    const getRecipesRandom = 'random';
+    return this.getQuery(getRecipesRandom).pipe(
+      map((resp:any[])=>
+        resp.map(recipe => {
+            console.log(recipe);
+        }))
+    )
+    
   }
 }
